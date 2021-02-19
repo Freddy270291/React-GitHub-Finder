a60cccdc42fd406b077b23b07909d1bb4d8805d8
Federico Giovannini <giovannini.federico@hotmail.it>
2021-02-19 20:26:57 +0100

    User and Repos state to Context

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import UserItem from "./userItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
+import GithubContext from "../../context/gitHub/githubContext";
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
   if (loading) {
     return <Spinner />;
   } else {
@@ -15,11 +19,6 @@ const Users = ({ users, loading }) => {
       </div>
     );
   }
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
 };
 
 const userStyle = {
